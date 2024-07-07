4cc902ca5774b74c19e28216ae9a70e36876a97b
Andrei Dziaineka <dziaineka@gmail.com>
2024-07-07 12:01:09 +0200

    add bot deps

--- mix.exs
+++ mix.exs
@@ -22,8 +22,10 @@ defmodule Lolek.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_gram, "~> 0.53"},
+      {:tesla, "~> 1.11"},
+      {:hackney, "~> 1.20"},
+      {:jason, ">= 1.4.0"}
     ]
   end
 end

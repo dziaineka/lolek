8d2cb96996d0a0bab7e432079f5f7d841e637f64
Andrei Dziaineka <dziaineka@gmail.com>
2026-01-11 15:40:34 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lolek.MixProject do
   def project do
     [
       app: :lolek,
-      version: "1.3.2",
+      version: "1.3.3",
       elixir: "1.19.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()

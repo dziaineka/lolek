d34c6abfd53dae9590525df9240b793eb3e5287d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-29 17:37:32 -0400

    Expose tests as Nix flake checks

--- flake.nix
+++ flake.nix
@@ -16,20 +16,31 @@
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (
+      perSystem =
         system:
         let
           pkgs = import nixpkgs { inherit system; };
           lib = pkgs.lib;
           beamPackages = pkgs.beam.packages.erlang_28;
-          mixRelease = beamPackages.mixRelease.override {
-            rebar3 = beamPackages.rebar3WithPlugins {
-              globalPlugins = [ beamPackages.pc ];
-            };
+          elixir = beamPackages.elixir_1_19;
+          rebar3WithPlugins = beamPackages.rebar3WithPlugins {
+            globalPlugins = [ beamPackages.pc ];
           };
-
+          fetchMixDeps = beamPackages.fetchMixDeps.override {
+            rebar3 = rebar3WithPlugins;
+          };
+          mixRelease = beamPackages.mixRelease.override {
+            rebar3 = rebar3WithPlugins;
+          };
+          sourceFiles = extraFiles: lib.fileset.unions ([ ./config ./lib ./mix.exs ./mix.lock ] ++ extraFiles);
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = sourceFiles [ ./rel ];
+          };
+          testSrc = lib.fileset.toSource {
+            root = ./.;
+            fileset = sourceFiles [ ./test ];
+          };
           runtimePath = lib.makeBinPath [
             pkgs.curl
             pkgs.ffmpeg
@@ -37,30 +48,48 @@
           ];
         in
         {
+          inherit
+            pkgs
+            lib
+            beamPackages
+            elixir
+            rebar3WithPlugins
+            fetchMixDeps
+            mixRelease
+            src
+            testSrc
+            runtimePath
+            ;
+        };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          inherit (perSystem system)
+            lib
+            elixir
+            fetchMixDeps
+            mixRelease
+            runtimePath
+            src
+            ;
+        in
+        {
           lolek = mixRelease {
             pname = "lolek";
             version = "1.8.1";
-            elixir = beamPackages.elixir_1_19;
+            inherit elixir;
 
-            src = lib.fileset.toSource {
-              root = ./.;
-              fileset = lib.fileset.unions [
-                ./config
-                ./lib
-                ./mix.exs
-                ./mix.lock
-                ./rel
-              ];
-            };
+            inherit src;
 
-            mixFodDeps = beamPackages.fetchMixDeps {
+            mixFodDeps = fetchMixDeps {
               pname = "lolek-mix-deps";
               version = "1.8.1";
               src = ./.;
+              inherit elixir;
               hash = "sha256-pdh+PiriuRixsEw2Mvjop3kTRyUo60mWdcB3PWhkqK8=";
             };
-
-            nativeBuildInputs = [ pkgs.makeWrapper ];
 
             postInstall = ''
               wrapProgram "$out/bin/lolek" \
@@ -78,6 +107,78 @@
           };
 
           default = self.packages.${system}.lolek;
+        }
+      );
+
+      checks = forAllSystems (
+        system:
+        let
+          inherit (perSystem system)
+            pkgs
+            beamPackages
+            elixir
+            rebar3WithPlugins
+            fetchMixDeps
+            testSrc
+            ;
+          testMixDeps = fetchMixDeps {
+            pname = "lolek-test-mix-deps";
+            version = "1.8.1";
+            src = ./.;
+            mixEnv = "test";
+            inherit elixir;
+            hash = "sha256-fiKeK0DCZZiwE0qhuD+BruI7PHkHPaxv9ivvfti1FYU=";
+          };
+        in
+        {
+          test = pkgs.stdenv.mkDerivation {
+            pname = "lolek-test";
+            version = "1.8.1";
+            src = testSrc;
+
+            nativeBuildInputs = [
+              elixir
+              (beamPackages.hex.override { inherit elixir; })
+              beamPackages.rebar
+              rebar3WithPlugins
+            ];
+
+            env = {
+              MIX_ENV = "test";
+              HEX_OFFLINE = 1;
+              MIX_REBAR = "${beamPackages.rebar}/bin/rebar";
+              MIX_REBAR3 = "${rebar3WithPlugins}/bin/rebar3";
+              LANG = if pkgs.stdenv.hostPlatform.isLinux then "C.UTF-8" else "C";
+              LC_CTYPE = if pkgs.stdenv.hostPlatform.isLinux then "C.UTF-8" else "UTF-8";
+            };
+
+            configurePhase = ''
+              runHook preConfigure
+
+              export MIX_HOME="$TMPDIR/mix"
+              export HEX_HOME="$TMPDIR/hex"
+              export MIX_DEPS_PATH="$TMPDIR/deps"
+              export REBAR_GLOBAL_CONFIG_DIR="$TMPDIR/rebar3"
+              export REBAR_CACHE_DIR="$TMPDIR/rebar3.cache"
+
+              cp --no-preserve=mode -R "${testMixDeps}" "$MIX_DEPS_PATH"
+
+              runHook postConfigure
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              mix test
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              touch "$out/passed"
+              runHook postInstall
+            '';
+          };
         }
       );
     };

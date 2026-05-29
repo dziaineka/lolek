8ef3f2eab4f6c0d48c744ffd0d8c1efbaab0e759
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-29 19:03:27 -0400

    Add download cache size setting

--- config/runtime.exs
+++ config/runtime.exs
@@ -12,6 +12,10 @@ config :lolek, :bot_token, :dotenv_config.get("LOLEK_BOT_TOKEN")
 config :lolek, :download_path, :dotenv_config.get("LOLEK_DOWNLOAD_DIR_PATH")
 
 config :lolek,
+       :max_download_dir_size,
+       :dotenv_config.get("LOLEK_MAX_DOWNLOAD_DIR_SIZE")
+
+config :lolek,
        :max_file_size_to_send_to_telegram,
        :dotenv_config.get("LOLEK_MAX_FILE_SIZE_TO_SEND_TO_TELEGRAM")
 

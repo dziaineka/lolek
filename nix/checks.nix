41b0dc026ad078488c20a96f2ab6d9a8d4981387
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-30 14:13:39 -0400

    refactor: inline package nix setup and drop common module

--- nix/checks.nix
+++ nix/checks.nix
@@ -5,67 +5,7 @@
   package ? null,
 }:
 
-let
-  common = import ./common.nix { inherit pkgs root; };
-
-  testMixDeps = common.fetchMixDeps {
-    pname = "lolek-test-mix-deps";
-    version = "1.8.1";
-    src = root;
-    mixEnv = "test";
-    inherit (common) elixir;
-    hash = "sha256-fiKeK0DCZZiwE0qhuD+BruI7PHkHPaxv9ivvfti1FYU=";
-  };
-in
 {
-  test = pkgs.stdenv.mkDerivation {
-    pname = "lolek-test";
-    version = "1.8.1";
-    src = common.testSrc;
-
-    nativeBuildInputs = [
-      common.elixir
-      (common.beamPackages.hex.override { inherit (common) elixir; })
-      common.beamPackages.rebar
-      common.rebar3WithPlugins
-    ];
-
-    env = {
-      MIX_ENV = "test";
-      HEX_OFFLINE = 1;
-      MIX_REBAR = "${common.beamPackages.rebar}/bin/rebar";
-      MIX_REBAR3 = "${common.rebar3WithPlugins}/bin/rebar3";
-      LANG = if pkgs.stdenv.hostPlatform.isLinux then "C.UTF-8" else "C";
-      LC_CTYPE = if pkgs.stdenv.hostPlatform.isLinux then "C.UTF-8" else "UTF-8";
-    };
-
-    configurePhase = ''
-      runHook preConfigure
-
-      export MIX_HOME="$TMPDIR/mix"
-      export HEX_HOME="$TMPDIR/hex"
-      export MIX_DEPS_PATH="$TMPDIR/deps"
-      export REBAR_GLOBAL_CONFIG_DIR="$TMPDIR/rebar3"
-      export REBAR_CACHE_DIR="$TMPDIR/rebar3.cache"
-
-      cp --no-preserve=mode -R "${testMixDeps}" "$MIX_DEPS_PATH"
-
-      runHook postConfigure
-    '';
-
-    buildPhase = ''
-      runHook preBuild
-      mix test
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p "$out"
-      touch "$out/passed"
-      runHook postInstall
-    '';
-  };
 }
 // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   nixos-service = import ./tests/service.nix {
--- nix/package.nix
+++ nix/package.nix
@@ -5,28 +5,74 @@
 }:
 
 let
-  common = import ./common.nix { inherit pkgs root; };
   lib = pkgs.lib;
+  beamPackages = pkgs.beam.packages.erlang_28;
+  elixir = beamPackages.elixir_1_19;
+  rebar3WithPlugins = beamPackages.rebar3WithPlugins {
+    globalPlugins = [ beamPackages.pc ];
+  };
+  fetchMixDeps = beamPackages.fetchMixDeps.override {
+    rebar3 = rebar3WithPlugins;
+  };
+  mixRelease = beamPackages.mixRelease.override {
+    makeWrapper = pkgs.makeBinaryWrapper;
+    rebar3 = rebar3WithPlugins;
+  };
+  sourceFiles =
+    extraFiles:
+    lib.fileset.unions (
+      [
+        (root + "/config")
+        (root + "/lib")
+        (root + "/mix.exs")
+        (root + "/mix.lock")
+      ]
+      ++ extraFiles
+    );
+  src = lib.fileset.toSource {
+    inherit root;
+    fileset = sourceFiles [ (root + "/rel") ];
+  };
   version = "1.8.1";
   runtimePath = lib.makeBinPath [
     pkgs.curl
     pkgs.ffmpeg-full
     pkgs.yt-dlp
   ];
+  testSrc = lib.fileset.toSource {
+    inherit root;
+    fileset = sourceFiles [ (root + "/test") ];
+  };
 in
 rec {
-  lolek = common.mixRelease {
+  lolek = mixRelease {
     pname = "lolek";
     inherit version;
-    inherit (common) elixir src;
+    inherit elixir src;
 
-    mixFodDeps = common.fetchMixDeps {
+    mixFodDeps = fetchMixDeps {
       pname = "lolek-mix-deps";
       inherit version;
       src = root;
-      inherit (common) elixir;
+      inherit elixir;
       hash = "sha256-pdh+PiriuRixsEw2Mvjop3kTRyUo60mWdcB3PWhkqK8=";
     };
+    doCheck = true;
+    checkPhase = ''
+      runHook preCheck
+
+      export MIX_ENV="prod"
+      export MIX_HOME="$TMPDIR/mix"
+      export HEX_HOME="$TMPDIR/hex"
+      export MIX_DEPS_PATH="$TMPDIR/deps"
+      export REBAR_GLOBAL_CONFIG_DIR="$TMPDIR/rebar3"
+      export REBAR_CACHE_DIR="$TMPDIR/rebar3.cache"
+
+      cp -R ${testSrc}/test test
+      mix test
+
+      runHook postCheck
+    '';
 
     postInstall = ''
       cat >> "$out/releases/${version}/env.sh" <<'EOF'

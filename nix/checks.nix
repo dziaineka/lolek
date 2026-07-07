a6a29aaa78a5d2e15d539ca71bb52cc9877e9c3d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-07-06 20:55:07 -0400

    Add Mix check to flake checks

--- nix/checks.nix
+++ nix/checks.nix
@@ -5,7 +5,63 @@
   package ? null,
 }:
 
+let
+  lib = pkgs.lib;
+  beamPackages = pkgs.beam.packages.erlang_29;
+  elixir = beamPackages.elixir_1_20;
+  rebar3WithPlugins = beamPackages.rebar3WithPlugins {
+    globalPlugins = [ beamPackages.pc ];
+  };
+  fetchMixDeps = beamPackages.fetchMixDeps.override {
+    rebar3 = rebar3WithPlugins;
+  };
+  mixRelease = beamPackages.mixRelease.override {
+    makeWrapper = pkgs.makeBinaryWrapper;
+    rebar3 = rebar3WithPlugins;
+  };
+  version = "5.0.1";
+  mixCheckSrc = lib.fileset.toSource {
+    inherit root;
+    fileset = lib.fileset.gitTracked root;
+  };
+  mixCheckDeps = fetchMixDeps {
+    pname = "lolek-mix-check-deps";
+    inherit version elixir;
+    src = root;
+    mixEnv = "dev";
+    hash = "sha256-0S2HHMQz3JBJx2xdHf1+yqqXXZOkATzJWjg2Q9LupDc=";
+  };
+in
 {
+  mix-check = mixRelease {
+    pname = "lolek-mix-check";
+    inherit version elixir;
+    src = mixCheckSrc;
+    mixEnv = "dev";
+    mixFodDeps = mixCheckDeps;
+    erlangDeterministicBuilds = false;
+    nativeBuildInputs = [
+      pkgs.getconf
+      pkgs.writableTmpDirAsHomeHook
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      export LOLEK_BOT_TOKEN="test_token"
+      mix check
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      touch "$out"
+
+      runHook postInstall
+    '';
+  };
 }
 // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   nixos-service = import ./tests/service.nix {
